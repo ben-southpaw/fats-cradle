75231df8d64c829cc547df597376727f5a607c3a
Ben Dryden <ben.dryden@gmail.com>
2025-03-20 14:45:22 +0100

    postMessage test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,48 @@
 <script>
 	import Canvas from '$lib/components/Canvas.svelte';
 	import ThreeScene from '$lib/components/ThreeScene.svelte';
+	import { onMount } from 'svelte';
 
 	let isTransitioning = false;
 	let canvasComponent;
+	let parentDimensions = null;
+	let isReady = false;
+
+	onMount(() => {
+		// Listen for messages from parent
+		window.addEventListener('message', (event) => {
+			// Verify origin for security
+			if (event.origin === 'https://fatscradle.com') {
+				const data = event.data;
+
+				// Check if it's a dimensions message
+				if (data.type === 'dimensions') {
+					parentDimensions = {
+						width: data.width,
+						height: data.height,
+					};
+
+					// Now we have dimensions, we can show the canvas
+					isReady = true;
+				}
+
+				console.log('received message', parentDimensions, 'yay');
+			}
+		});
+
+		// Request dimensions from parent
+		window.parent.postMessage(
+			{ type: 'requestDimensions' },
+			'https://fatscradle.com'
+		);
+
+		// Fallback - if we don't get parent dimensions within 1 second, show anyway
+		setTimeout(() => {
+			if (!isReady) {
+				isReady = true;
+			}
+		}, 1000);
+	});
 
 	function handleTransitionStart() {
 		isTransitioning = true;
@@ -18,8 +57,14 @@
 </script>
 
 <section>
-	<Canvas bind:this={canvasComponent} />
-	<ThreeScene on:transitionstart={handleTransitionStart} on:wipe={handleWipe} />
+	{#if isReady}
+		<Canvas bind:this={canvasComponent} {parentDimensions} />
+		<ThreeScene
+			on:transitionstart={handleTransitionStart}
+			on:wipe={handleWipe}
+			{parentDimensions}
+		/>
+	{/if}
 </section>
 
 <style>
